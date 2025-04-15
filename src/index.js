84a1ff535da1b4a6359c3f7936634e4704a89b7f
Karthik564125 <2200030209@kluniversity.in>
2025-04-15 23:19:23 +0530

    Fixed unused Router import to resolve build error

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { HashRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
